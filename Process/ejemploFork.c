d9af5eba848cbceaa3c61c9c330ef29c722d6ebb
Fir <xblackcat93@gmail.com>
2024-09-30 01:16:22 +0200

    fixed merge spaghetti

--- Process/ejemploFork.c
+++ Process/ejemploFork.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -61,33 +61,3 @@
  * Haciendo que el padre sea su ancestro directo
  *
  * El PPID del padre sera diferente porque se refiere al proceso que ha creado al padre (el shell, terminal) */
-=======
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <stdio.h>
-
-int main(){
-/*tipo int que representa el ID de un proceso*/
-pid_t pid;
-int status;
-
-/*Creamos un proceso*/
-pid=fork();
-if (pid==-1){
-	printf("no se ha creado un proceso");
-	exit(-1);
-}
-
-if (pid==0) {
-	printf("soy hijo\n%d\n", getpid()); /*proceso hijo*/
-	exit(0);
-} else{
-	wait(NULL);
-	printf("soy padre \n%d\n", getppid()); /*proceso padre*/
-}
-}
-
-
->>>>>>> d6b9c828450797260491257df7da483851398f88
